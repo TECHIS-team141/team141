3c7093ad51fcbc5015e07d8ac35f491f15b1a694
sayaaaka <ange.chat69@gmail.com>
2023-02-26 17:15:07 +0900

    20230226_sayaka2

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link href="/css/style_ono.css" rel="stylesheet">
+    <link href="/css/style_sayaka.css" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
